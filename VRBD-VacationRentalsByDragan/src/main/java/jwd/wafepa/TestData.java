e5e7f50fbba2ce0cac121057e3705d5674f659e9
User <User@User-PC>
2020-01-07 12:44:06 +0100

    The beginning

---
+++ VRBD-VacationRentalsByDragan/src/main/java/jwd/wafepa/TestData.java
@@ -0,0 +1,21 @@
+package jwd.wafepa;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestData {
+	
+
+
+	
+	@PostConstruct
+	public void init() {
+		
+	
+		
+		
+	}
+
+}
